e86137ce3c70be931e9ae2661a355d47fb01254a
Dart1516 <Danvddpf1@gmail.com>
2024-04-05 04:07:26 -0300

    data base updated

--- scripts/random-vehicles.js
+++ scripts/random-vehicles.js
@@ -1,6 +1,6 @@
-const url = 'https://github.com/Dart1516/Rental-Scoots-Site/blob/main/data/maxrentalpricing.json'
+const url = 'https://raw.githubusercontent.com/Dart1516/Rental-Scoots-Site/main/data/maxrentalpricing.json';
 
-async function getData(url){
+async function getData(url) {
     try {
         const response = await fetch(url);
         return await response.json();
@@ -8,3 +8,27 @@ async function getData(url){
         console.error('Error fetching JSON:', error);
     }
 }
+
+function getRandomVehicle(data) {
+    const rentalTypes = Object.keys(data.rentalType);
+    const randomType = rentalTypes[Math.floor(Math.random() * rentalTypes.length)];
+    const vehicle = data.rentalType[randomType];
+    const colors = Object.keys(vehicle.color);
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    const selectedColor = vehicle.color[randomColor];
+    return {
+        name: randomType,
+        description: vehicle.description,
+        imageURL: selectedColor.imageURL
+    };
+}
+
+async function displayRandomVehicle() {
+    const jsonData = await getData(url);
+    const randomVehicle = getRandomVehicle(jsonData);
+    document.getElementById('vehicle-name').textContent = randomVehicle.name;
+    document.getElementById('vehicle-image').src = randomVehicle.imageURL;
+    document.getElementById('vehicle-description').textContent = randomVehicle.description;
+}
+
+displayRandomVehicle();
