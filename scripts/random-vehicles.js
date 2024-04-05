468b025078e42dc79ffc8cbbcc57b51163c07799
Dart1516 <Danvddpf1@gmail.com>
2024-04-05 04:36:30 -0300

    data updated

--- scripts/random-vehicles.js
+++ scripts/random-vehicles.js
@@ -17,12 +17,11 @@ function getRandomVehicle(data) {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     const selectedColor = vehicle.color[randomColor];
     return {
-        name: vehicle.name, 
+        name: vehicle.name,
         description: vehicle.description,
         imageURL: selectedColor.imageURL
     };
 }
-
 
 async function displayRandomVehicle() {
     const jsonData = await getData(url);
